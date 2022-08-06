4026da7087b6ed0ea2d92e6c3e472443711d2793
david <davidoluwasemiloorei@gmail.com>
2022-08-06 10:40:31 +0100

    js-basics

---
+++ index.js
@@ -0,0 +1,72 @@
+
+    //    VARIABLES
+// let name = 'David';
+// console.log (name);
+
+// let firstName = 'David'
+// let lastName = 'Irefin'
+// console.log (firstName + " " + lastName);
+
+
+   //    CONSTANT
+// const interestRate = 0.3;
+// console.log (interestRate);
+
+
+//    OBJECT
+// let person = {
+//     name : 'David',
+//     age : 30
+// };
+// console.log(person);
+
+
+//    DOT NOTATION
+// person.name = 'John';
+// console.log(person.name);
+
+
+//    BRACKET NOTATION
+// person['name'] = 'Mary';
+// console.log(person.name);
+
+
+//   ARRAYS
+// let selectedColors = ['red', 'blue'];
+// console.log(selectedColors);
+
+
+//    How to add third element in an array
+// let selectedColors = ['red', 'blue'];
+// selectedColors [2] = 'green';
+// console.log(selectedColors);
+
+
+//    Length property
+// let selectedColors = ['red', 'blue'];
+// selectedColors [2] = 1;
+// console.log(selectedColors);
+
+
+//    FUNCTION
+// function greet () {
+//     console.log ('Hello World');
+// }
+
+// greet ();
+
+
+//    Multiple Parameters {performing a task}
+// function greet (name, lastName) {
+//     console.log('Hello ' + name + ' ' + lastName)
+// }
+
+// greet('John', 'Smith');
+
+
+     //  Calculating a value
+function square(number, digits) {
+    return number * number + digits;
+}
+
+console.log(square(4, 2));
